5b84440df77a733ecd27f5311d15f4090fc73fe0
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-07-22 10:45:44 +0200

    Implement `LambdaSite` tag

---
+++ src/Lambda.jl
@@ -0,0 +1,19 @@
+struct LambdaSite{B<:Bond} <: Site
+    bond::B
+end
+
+# required for set-like equivalence of `Bond` to work on dictionaries
+Base.isequal(s1::LambdaSite, s2::LambdaSite) = isequal(s1.bond, s2.bond)
+Base.hash(s::LambdaSite, h::UInt) = hash(s.bond, h) ⊻ hash("LambdaSite", h)
+
+bond(s::LambdaSite) = bond(s.bond)
+sites(s::LambdaSite) = sites(bond(s))
+
+dispatch_site_constructor(b::Bond) = LambdaSite(b)
+
+macro lambda_str(str)
+    bondexpr = Expr(:macrocall, Symbol("@bond_str"), __source__, str)
+    :(LambdaSite($bondexpr))
+end
+
+Base.show(io::IO, x::LambdaSite) = print(io, "site<$(x.bond)>")
